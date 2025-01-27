000e2c841251b7feed749b6256d64d8027ede032
Catherine Herzog <catherine.herzog@gmail.com>
2025-01-27 11:15:05 -0700

    removing roxygen example in validate_gen_determ_input

--- compModels/R/validate_gen_determ_input.R
+++ compModels/R/validate_gen_determ_input.R
@@ -8,26 +8,6 @@
 #' @param comp_names names of the compartments
 #' @param trans_matrix matrix of transition rates between each of the
 #' @return stops with information or indicates Input Checks Passed
-#' @examples
-#' \dontrun{
-#' validate_gen_determ_input(
-#'   init_vals = c(1e05 - 1, 10, 5, 1, 0),
-#'   times = seq(0.1, 100, by = 0.1),
-#'   comp_names = c("A", "B", "C", "D", "E"),
-#'   trans_matrix <- matrix(
-#'     c(
-#'       -0.1, 0.1, 0, 0, 0,
-#'       0, -0.1, 0.1, 0, 0,
-#'       0, 0, -0.1, 0.1, 0,
-#'       0, 0, 0, -0.1, 0.1,
-#'       0.1, 0, 0, 0, -0.1
-#'     ),
-#'     nrow = length(c("A", "B", "C", "D", "E")),
-#'     byrow = TRUE,
-#'     dimnames = list(c("A", "B", "C", "D", "E"), c("A", "B", "C", "D", "E"))
-#'   )
-#' )
-#' }
 validate_gen_determ_input <- function(init_vals, times,
                                       comp_names, trans_matrix, subgroups_list,
                                       intervention_start_time,
