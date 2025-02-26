3c399bca5b4fa3b842afe1c1288f06ef6b2b1282
Catherine M Herzog <14792937+cherz4@users.noreply.github.com>
2025-02-26 10:48:08 -0700

    Explicitly naming t in generalized_rates() environment (#71)
    Resolves issue #70
    
    ---------
    
    Co-authored-by: Beau Bruce <bbbruce@users.noreply.github.com>

--- compModels/R/generalized_rates.r
+++ compModels/R/generalized_rates.r
@@ -3,14 +3,15 @@
 #' Helper function to calculate modified change rates for in and out flows in
 #' a stochastic generalized compartmental model using [adaptivetau::ssa.exact()]
 #' @param rate_eqns a list of the transition rate equations between flows,
-#'                  expressed with dplyr::expr() and listed in the same order
-#'                  as the transitions supplied to the model runner function
+#'                  listed in the same order as the transitions supplied to the
+#'                  model runner function
 #' @return the rate(s) at a specific time point
 #' @export
 
 generalized_rates <- function(rate_eqns) {
   function(state, params, t) {
     sapply(rate_eqns, function(eq) {
+      t <- c(t = t)
       eval(parse(text = eq), envir = as.list(c(state, params, t)))
     })
   }
