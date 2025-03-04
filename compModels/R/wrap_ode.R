9e2606d6eda342716a44eaee7a901c4e2fb0dbd4
Catherine M Herzog <14792937+cherz4@users.noreply.github.com>
2025-03-04 09:55:15 -0700

    Issue 85: create deSolve::ode() wrapper function (#86)
    Resolves Issue #85 
    
    Welcome feedback on what test(s) we might want to add to this function.

---
+++ compModels/R/wrap_ode.R
@@ -0,0 +1,34 @@
+#' Wrapper to run [deSolve::ode()] from compiled model and formatted inputs
+#'
+#' @param y the initial (state) values for the ODE system, a named vector
+#' @param times vector of sequence of times for which output is wanted; first
+#' value must be the initial time
+#' @param compiledmodel compiled model as a list, output from compilemodel()
+#' @param parms named vector of parameters passed to the ode function created by
+#' model2desolvefunction using the compiledmodel
+#' @return deSolve output run on provided input
+#' @export
+#' @examples
+#' \dontrun{
+#' base_states <- c("S", "I", "R")
+#' sir <- define_states(base_states) |>
+#'   add_infection("I", "S", "I", "beta") |>
+#'   add_transition("I", "R", "tau")
+#' sircompiled <- compilemodel(sir)
+#' wrap_ode(
+#'   c("S" = 999, "I" = 1, "R" = 0),
+#'   seq(0, 25, by = 0.01),
+#'   sircompiled,
+#'   c(beta = 2, tau = 1)
+#' )
+#' #'
+#' }
+wrap_ode <- function(y, times, compiledmodel, parms) {
+  odefun <- model2desolvefunction(compiledmodel)
+  x0 <- define_initialstate(compiledmodel, y) |>
+    output_initialstate()
+  parameters <- parms
+  times <- times
+  dyn <- ode(y = x0, times = times, func = odefun, parms = parameters)
+  return(dyn)
+}
